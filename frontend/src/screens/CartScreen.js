3b2976d88e87127c716a101e8312f3d3cd1406c1
ramira4 <ramireza42213@yahoo.com>
2022-01-07 00:24:41 -0800

    commit with cart ui

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -30,8 +30,63 @@ const CartScreen = {
                 qty: 1,
             });
         }
-        return `<div>Cart Screen</div>
-        <div>${getCartItems().length}</div>`;
+        const cartItems = getCartItems();
+        return `
+        <div class="content cart">
+            <div class="cart-list">
+                <ul class="cart-list-container">
+                    <li>
+                        <h3>Shopping Cart</h3>
+                        <div>Price</div>
+                    </li>
+                    <li> 
+                    ${
+                        cartItems.length === 0?
+                        '<div> Cart is empty. <a href="/#/">Go Shopping</a></div>':
+                        cartItems.map(item => `
+                            <li>
+                                <div class="cart-image">
+                                    <img src="${item.image}" alt="${item.name}"/>
+                                </div>
+                                <div class="cart-name">
+                                <div>
+                                    <a href="/#/product/${item.product}">
+                                    ${item.name}
+                                    </a>
+                                </div>
+                                <div>
+                                    Qty: 
+                                    <select class="qty-select" id="${item.product}">
+                                    <option value="1">1</option>
+                                    </select>
+                                    <button type="button" class="delete=button" id="${item.product}">
+                                    Delete
+                                    </button>
+    
+                                </div>
+                                <div class="cart-price">
+                                    $${item.price}
+                                </div>
+                            </li> `
+                            )
+                            .join('\n')
+                        }
+                    </li>
+                </ul>
+            </div>
+            <div class="cart-action">
+                <h3>
+                Subtotal (${cartItems.reduce((a,c) => a+c.qty,0)} items) 
+                :
+                $${cartItems.reduce((a,c)=> a+c.price*c.qty, 0 )}
+                </h3>
+                <button id="checkout-button" class="primary fw">
+                Proceed to Checkout
+                </button>
+                
+            </div>
+        </div>
+        `;
     },
 };
 export default CartScreen;
